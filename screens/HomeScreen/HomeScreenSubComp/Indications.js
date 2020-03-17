aa795723f020a058b65aa4980e746ec114ebadb1
amoriceau <contact.amoriceau@gmail.com>
2020-03-17 17:07:36 +0100

    v.0.5 indication component

--- screens/HomeScreen/HomeScreenSubComp/Indications.js
+++ screens/HomeScreen/HomeScreenSubComp/Indications.js
@@ -1,15 +1,50 @@
 import * as React from 'react';
-import { View, Text,Platform,StyleSheet } from 'react-native';
+import { View, Text, Platform, StyleSheet, ScrollView } from 'react-native';
 
 import Colors from '../../../constants/Colors';
+import Layout from '../../../constants/Layout';
 
 import Header, { headerHeight } from '../../../components/Header';
+import ParrallaxContent from '../../../components/parralaxContent';
+
 const Indication = ({ navigation, route }) => {
 	return (
 		<View style={styles.container}>
 			<Header navigation={navigation} route={route} />
 			<View style={styles.body}>
-				<Text>Info !</Text>
+				<ScrollView horizontal pagingEnabled>
+					<ParrallaxContent
+						picture={
+							'https://images.unsplash.com/photo-1583324113626-70df0f4deaab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80'
+						}
+						titre={"Le Coronavirus"}
+					/>
+					<ParrallaxContent
+						picture={
+							'https://images.unsplash.com/photo-1583947582886-f40ec95dd752?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+						}
+						titre={'Se laver les mains'}
+					/>
+
+					<ParrallaxContent
+						picture={
+							'https://images.unsplash.com/photo-1580893246395-52aead8960dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+						}
+						titre={"Évitez les contacts"}
+					/>
+					<ParrallaxContent
+						picture={
+							'https://images.unsplash.com/photo-1583947215172-b3880c534af4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjF9&auto=format&fit=crop&w=750&q=80'
+						}
+						titre={'Utilisez des mouchoirs en papier'}
+					/>
+					<ParrallaxContent
+						picture={
+							'https://images.unsplash.com/photo-1512479064533-47d51d07bb93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+						}
+						titre={"Portez un masque"}
+					/>
+				</ScrollView>
 			</View>
 		</View>
 	);
